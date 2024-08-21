9bac69760f1fbe6fba3274864ec087c5b11ec3c6
robperez-dev <robperez-dev@users.noreply.github.com>
2024-08-21 10:14:31 -0400

    Add Footer Add Questions JS

---
+++ js/questions.js
@@ -0,0 +1,18 @@
+(function(){
+  const titleQuestions=[...document.querySelectorAll('.questions__title')];
+  titleQuestions.forEach(question=>{
+    question.addEventListener('click',()=>{
+      let height=0;
+      const answer=question.nextElementSibling;
+      let addPadding =question.parentElement.parentElement;
+
+      addPadding.classList.toggle('questions__padding--add');
+      question.children[0].classList.toggle('questions__arrow--rotate');
+
+      if (answer.clientHeight===0){
+        height=answer.scrollHeight;
+      }
+      answer.style.height=`${height}px`;
+    });
+  });
+})();
